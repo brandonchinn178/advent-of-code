94b724f39a27a71cdabfdfe0f916ba39bf72aed9
Brandon Chinn <brandonchinn178@gmail.com>
2021-12-01 21:52:57 -0800

    2021, Day 2

---
+++ 2021/Day2.hs
@@ -0,0 +1,50 @@
+{-
+stack script --resolver lts-18.18
+-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+
+data Command
+  = Forward Int
+  | Up Int
+  | Down Int
+  deriving (Show)
+
+parseCommand :: String -> Command
+parseCommand s =
+  case words s of
+    ["forward", x] -> Forward (read x)
+    ["up", x] -> Up (read x)
+    ["down", x] -> Down (read x)
+    _ -> error $ "Invalid input: " ++ s
+
+data State = State
+  { position :: Int
+  , depth :: Int
+  , aim :: Int
+  }
+
+runCommands :: (State -> Command -> State) -> [Command] -> State
+runCommands f = foldl f (State 0 0 0)
+
+calcResult :: State -> Int
+calcResult State{..} = position * depth
+
+main :: IO ()
+main = do
+  input <- map parseCommand . lines <$> readFile "Day2.txt"
+  -- let input = [Forward 5, Down 5, Forward 8, Up 3, Down 8, Forward 2]
+
+  let runWith f = print . calcResult . runCommands f $ input
+
+  -- part 1
+  runWith $ \state@State{..} -> \case
+    Forward x -> state{position = position + x}
+    Up x -> state{depth = depth - x}
+    Down x -> state{depth = depth + x}
+
+  -- part 2
+  runWith $ \state@State{..} -> \case
+    Forward x -> state{position = position + x, depth = depth + x * aim}
+    Down x -> state{aim = aim + x}
+    Up x -> state{aim = aim - x}
