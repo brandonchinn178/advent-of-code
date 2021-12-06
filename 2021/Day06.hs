ec3163ecc1bba3a1472b2dc88b829e30fee18637
Brandon Chinn <brandonchinn178@gmail.com>
2021-12-05 22:20:08 -0800

    2021, Day 6 p2

--- 2021/Day06.hs
+++ 2021/Day06.hs
@@ -5,20 +5,37 @@ stack script --resolver lts-18.18
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE TypeApplications #-}
 
-import Data.List (iterate')
 import Data.Text qualified as Text
 
 main :: IO ()
 main = do
   input <- map (read @Int) . splitOn "," <$> readFile "Day06.txt"
 
-  -- part 1
-  print $ length $ (!! 80) $ iterate' tick input
+  let countTotalFishAtDay x = sum $ map (\c -> totalFishFrom c !! x) input
 
-tick :: [Int] -> [Int]
-tick = concatMap tickFish
-  where
-    tickFish x = if x == 0 then [6, 8] else [x - 1]
+  -- part 1
+  print $ countTotalFishAtDay 80
+
+  -- part 2
+  print $ countTotalFishAtDay 256
+
+-- `totalFishFrom c !! x` represents the total number of active fish
+-- at day `x` who descended from a single fish starting at an internal
+-- counter of `c`.
+--
+-- Includes the original fish in the count (so `totalFishFrom c !! 0 == 1`)
+-- and includes all fish birthed by fish birthed by the original fish (and so
+-- on).
+totalFishFrom :: Int -> [Int]
+totalFishFrom c = replicate (c + 1) 1 ++ zipWith (+) totalFishFrom6 totalFishFrom8
+
+-- memoize `totalFishFrom 6`
+totalFishFrom6 :: [Int]
+totalFishFrom6 = totalFishFrom 6
+
+-- memoize `totalFishFrom 8`
+totalFishFrom8 :: [Int]
+totalFishFrom8 = totalFishFrom 8
 
 {-- Utilities --}
 
