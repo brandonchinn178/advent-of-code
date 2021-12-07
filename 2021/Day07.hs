89c937b66f9416b36a89ad20b758a8ba11a5bd33
Brandon Chinn <brandonchinn178@gmail.com>
2021-12-06 21:16:59 -0800

    2021, Day 7

---
+++ 2021/Day07.hs
@@ -0,0 +1,31 @@
+{-
+stack script --resolver lts-18.18
+  --package text
+-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications #-}
+
+import Data.Text qualified as Text
+
+main :: IO ()
+main = do
+  input <- map (read @Int) . splitOn "," <$> readFile "Day07.txt"
+
+  -- part 1
+  print $ getBestFuelCostWith id input
+
+  -- part 2
+  print $ getBestFuelCostWith sumTo input
+
+getBestFuelCostWith :: (Int -> Int) -> [Int] -> Int
+getBestFuelCostWith f xs = minimum $ map fuelCostTo [0 .. maximum xs]
+  where
+    fuelCostTo pos = sum $ map (f . abs . subtract pos) xs
+
+sumTo :: Int -> Int
+sumTo n = (n * (n + 1)) `div` 2
+
+{-- Utilities --}
+
+splitOn :: String -> String -> [String]
+splitOn sep = map Text.unpack . Text.splitOn (Text.pack sep) . Text.pack
