1c01a4aa3595a109d3a9aa2dd8bdf4676c3eb68d
Brandon Chinn <brandonchinn178@gmail.com>
2021-12-11 13:13:22 -0800

    2021, Day 9 p1 rewrite with Matrix

--- 2021/Day09.hs
+++ 2021/Day09.hs
@@ -2,40 +2,51 @@
 stack script --resolver lts-18.18
 -}
 
+import Control.Monad ((>=>))
 import Data.Char (digitToInt)
-import Data.List (transpose)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, mapMaybe)
 
 main :: IO ()
 main = do
-  input <- map (map digitToInt) . lines <$> readFile "Day09.txt"
+  input <- Matrix . map (map digitToInt) . lines <$> readFile "Day09.txt"
 
   -- part 1
   print $ sum . map (+ 1) . getLowPoints $ input
 
-getLowPoints :: [[Int]] -> [Int]
-getLowPoints xs =
-  catMaybes . concat $
-    zipMatrices
-      (\x isLow -> if isLow then Just x else Nothing)
-      xs
-      lowPoints
-  where
-    lowPoints =
-      zipMatrices
-        (&&)
-        (map getLowPointsForRow $ xs)
-        (transpose . map getLowPointsForRow . transpose $ xs)
+getLowPoints :: Matrix Int -> [Int]
+getLowPoints m =
+  catMaybesMatrix $
+    mapMatrix
+      ( \coord x ->
+          if all (> x) $ mapMaybe (matrixGet m) (neighbors coord)
+            then Just x
+            else Nothing
+      )
+      m
 
-    zipMatrices :: (a -> b -> c) -> [[a]] -> [[b]] -> [[c]]
-    zipMatrices f = zipWith (zipWith f)
+{-- Matrix --}
 
-getLowPointsForRow :: [Int] -> [Bool]
-getLowPointsForRow xs =
-  zipWith3
-    (\pre x post -> x < pre && x < post)
-    (inf : xs)
-    xs
-    (tail xs ++ [inf])
+newtype Matrix a = Matrix {unMatrix :: [[a]]}
+
+type Coordinate = (Int, Int)
+
+mapMatrix :: (Coordinate -> a -> b) -> Matrix a -> Matrix b
+mapMatrix f = Matrix . withIndex (\y -> withIndex (\x -> f (x, y))) . unMatrix
   where
-    inf = 1000 -- all numbers in row are single digits
+    withIndex f = zipWith f [0 ..]
+
+catMaybesMatrix :: Matrix (Maybe a) -> [a]
+catMaybesMatrix = catMaybes . concat . unMatrix
+
+matrixGet :: Matrix a -> Coordinate -> Maybe a
+matrixGet m (x, y) = (arrayGet y >=> arrayGet x) . unMatrix $ m
+  where
+    arrayGet i xs = if 0 <= i && i < length xs then Just (xs !! i) else Nothing
+
+neighbors :: Coordinate -> [Coordinate]
+neighbors (x, y) =
+  [ (x, y - 1) -- up
+  , (x + 1, y) -- right
+  , (x, y + 1) -- down
+  , (x - 1, y) -- left
+  ]
