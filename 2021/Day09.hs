e68bb5ab7dc834dedca3ee8ac2a2d486c26263fa
Brandon Chinn <brandonchinn178@gmail.com>
2021-12-11 14:07:44 -0800

    2021, Day 9 p2

--- 2021/Day09.hs
+++ 2021/Day09.hs
@@ -1,28 +1,57 @@
 {-
 stack script --resolver lts-18.18
+  --package containers
 -}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TupleSections #-}
 
 import Control.Monad ((>=>))
 import Data.Char (digitToInt)
+import Data.List (sortOn)
 import Data.Maybe (catMaybes, mapMaybe)
+import Data.Ord (Down (..))
+import Data.Set (Set)
+import Data.Set qualified as Set
 
 main :: IO ()
 main = do
   input <- Matrix . map (map digitToInt) . lines <$> readFile "Day09.txt"
 
-  -- part 1
-  print $ sum . map (+ 1) . getLowPoints $ input
+  let lowPoints = getLowPoints input
 
-getLowPoints :: Matrix Int -> [Int]
+  -- part 1
+  print $ sum $ map ((+ 1) . snd) lowPoints
+
+  -- part 2
+  let basinSizes = map (getBasinSize input . fst) lowPoints
+  print $ product $ take 3 $ sortOn Down basinSizes
+
+getLowPoints :: Matrix Int -> [(Coordinate, Int)]
 getLowPoints m =
   catMaybesMatrix $
     mapMatrix
       ( \coord x ->
-          if all (> x) $ mapMaybe (matrixGet m) (neighbors coord)
-            then Just x
+          if all ((> x) . snd) (matrixNeighbors m coord)
+            then Just (coord, x)
             else Nothing
       )
       m
+
+getBasinSize :: Matrix Int -> Coordinate -> Int
+getBasinSize m coord0 = Set.size $ go (Set.singleton coord0) [coord0]
+  where
+    go seen [] = seen
+    go seen (coord:rest) =
+      let next =
+            [ c
+            | (c, x) <- matrixNeighbors m coord
+            , x < 9
+            , c `Set.notMember` seen
+            ]
+       in go (extend next seen) (next ++ rest)
+
+    extend :: Ord a => [a] -> Set a -> Set a
+    extend as = Set.union (Set.fromList as)
 
 {-- Matrix --}
 
@@ -43,10 +72,12 @@ matrixGet m (x, y) = (arrayGet y >=> arrayGet x) . unMatrix $ m
   where
     arrayGet i xs = if 0 <= i && i < length xs then Just (xs !! i) else Nothing
 
-neighbors :: Coordinate -> [Coordinate]
-neighbors (x, y) =
-  [ (x, y - 1) -- up
-  , (x + 1, y) -- right
-  , (x, y + 1) -- down
-  , (x - 1, y) -- left
-  ]
+matrixNeighbors :: Matrix a -> Coordinate -> [(Coordinate, a)]
+matrixNeighbors m (x, y) =
+  mapMaybe
+    (\coord -> (coord,) <$> matrixGet m coord)
+    [ (x, y - 1) -- up
+    , (x + 1, y) -- right
+    , (x, y + 1) -- down
+    , (x - 1, y) -- left
+    ]
