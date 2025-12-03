409b2c96b40e6dc2bc5204b07a25485feecc44f7
Brandon Chinn <brandonchinn178@gmail.com>
2025-12-03 10:22:53 -0800

    Update runner

--- 2025/run.sh
+++ 2025/run.sh
@@ -2,6 +2,8 @@
 
 set -eu -o pipefail
 
+outdir=build
+
 main() {
     local input=${1:-}; shift
 
@@ -22,29 +24,28 @@ main() {
     fi
 
     if [[ -f "${day}.sql" ]]; then
-        rm -rf .runner.db
-        sqlite3 .runner.db \
+        local db=${outdir}/sqlite/${day}.db
+        rm -rf "${db}"
+        mkdir -p "$(dirname "${db}")"
+        sqlite3 "${db}" \
             'create table input (line TEXT)' \
             '.mode csv' \
             '.headers off' \
             ".import ${input} input"
-        time sqlite3 .runner.db -bail < "${day}.sql"
+        time sqlite3 "${db}" -bail < "${day}.sql"
     fi
 
     if [[ -f "${day}.hs" ]]; then
-        run_stack --no-run "${day}.hs"
-        time run_stack "${day}.hs" -- +RTS -t -RTS < "${input}"
+        local outdir_hs="${outdir}/hs/${day}"
+        mkdir -p "${outdir_hs}"
+        ghc-9.12 -Wall -O3 \
+            -package text \
+            -odir "${outdir_hs}" \
+            -hidir "${outdir_hs}" \
+            -o "${outdir_hs}/Main" \
+            "${day}.hs"
+        time "${outdir_hs}/Main" +RTS -t -RTS < "${input}"
     fi
-}
-
-run_stack() {
-    stack script \
-        --optimize \
-        --use-root \
-        --ghc-options=-Wall \
-        --resolver=nightly-2025-12-01 \
-        --package text \
-        "$@"
 }
 
 main "$@"
